d708434beac7feed3512cda8c93dc777eaa61fd5
Eden Ungar <edenungar@gmail.com>
2018-06-06 10:53:19 -0400

    Worked on Relational Operators

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,9 +5,9 @@ public class Main {
     public static void main(String[] args) {
 	/*
 	    Relational Operators
-	    < - less than
+	    <  - less than
 	    <= -less than or equal to
-	    > - greater than
+	    >  - greater than
 	    >= - greater than or equal to
 	    == - equals
 	    != - not equal to
@@ -15,7 +15,7 @@ public class Main {
 	    Logical Operators
 	    && - and
 	    || - or
-	    ! - not
+	    !  - not
 	 */
 	int a = 2;
 	int b = 4;
@@ -30,17 +30,55 @@ public class Main {
 	// && (and) operator
 	if(a == x && y + y == a){
 	    System.out.println("a == x && y + y == a");
-	    //this method will not run because one of the conditions is not true, y + y does equal a, but a does not equal
+	    //this method WILL NOT run because one of the conditions is not true, y + y does equal a, but a does not equal
 		//x and the && needs both statements to be true
 	}
 	// || (or) operator
 	if(a == x || y + y == a){
 		System.out.println("a == x || y + y == a");
-		//this will work because the or || only needs one to be true
+		//this WILL run because the or || only needs one to be true
     }
     // ! (not) operator
     if(a != x && y + y == a){
 		System.out.println("a != x && y + y == a");
-		//this will run bc by using the ! (not) operator, this AND will evaluate as true on both sides because a does not equal x
+		//this WILL run bc by using the ! (not) operator, this AND will evaluate as true on both sides because a does not equal x
+	}
+//	// other examples
+//	if(a + 7 != 8 && a + b != y){
+//		System.out.println("a + 7 != 9 && a + b != y");
+//	}
+//	if(a > y || x < a) {
+//		System.out.println("a > y || x < a");
+//	}
+	// Relational Operators
+	// < (less than) operator
+	if(a < b){
+		System.out.println("a < b");
+		//this WILL run because 2(a) is less than 4(b)
+	}
+	// <= (less than or equal to) operator
+	if(a <= x){
+		System.out.println("a <= x");
+		//this WILL run because 2(a) is less than 6(x)
+	}
+	// > (greater than) operator
+	if(b > a){
+		System.out.println("b > a");
+		//this WILL run because 4(b) is greater than 2(a)
+	}
+	// >= (greater than or equal to) operator
+	if(y >= 1){
+		System.out.println("y >= 1");
+		//this WILL run because y(1) equals 1
+	}
+	// == (equal to) operator
+	if(x == 6){
+		System.out.println("x == 6");
+		//this WILL run because x(6) equals 6
+	}
+	// != (not equal to) operator
+	if(b != y){
+		System.out.println("b != y");
+		//this WILL run because b(4) does not equal y(1)
 	}
 }}
